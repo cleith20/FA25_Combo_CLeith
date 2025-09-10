740986a44b6b61e6c87c2566732d90c6f382b901
Leith <S03110369@ccc.ccofc.edu>
2025-09-10 13:57:12 -0600

    Practice

--- Practice/src/weather_stations.java
+++ Practice/src/weather_stations.java
@@ -5,13 +5,16 @@ public class weather_stations {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		int count = 0;
 		int goodCount = 3;
 		
-		double precip , proTotal , proAvg , total , cityAvg;
+		double precip, proTotal , proAvg , total , cityAvg;
 		
-	
+		for(int count = 0; count < 200;)
+		{
+			
+		}
 
+	
 	}
 
 }
