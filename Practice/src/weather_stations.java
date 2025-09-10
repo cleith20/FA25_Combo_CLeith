52fd643ffbaf60ca0292c04fd2f8920d8841e215
cleith20 <cameronjleith@gmail.com>
2025-09-10 10:26:57 -0600

    TicTacToe

--- Practice/src/weather_stations.java
+++ Practice/src/weather_stations.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class weather_stations {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		int count = 0;
+		int goodCount = 3;
+		
+		double precip , proTotal , proAvg , total , cityAvg;
+		
+	
 
 	}
 
