67225941d546efaa2fcde1505d748e234a568c81
Leith <S03110369@ccc.ccofc.edu>
2025-09-11 10:04:26 -0600

    tictactoe

---
+++ Practice/src/weather_stations.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class weather_stations {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int goodCount = 3;
+		
+		double precip, proTotal , proAvg , total , cityAvg;
+		
+		for(int count = 0; count < 200;)
+		{
+			
+		}
+
+	
+	}
+
+}
